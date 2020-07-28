be54302fc0c4b732e5e8e110a12ff1c7b951053b
Jonathan Crispiniano <jonathancrispiniano@gmail.com>
2020-07-28 18:26:43 -0300

    Adicionado arquivo de script home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,22 +1,5 @@
-let slideIndex = 0;
-
-const showSlides = () => {
-   
-    const slides = document.getElementsByClassName("vitrine-media");
-
-    for(let i=0; i < slides.length; i++) {
-        slides[i].getElementsByClassName.display="none";
-    }
-
-    slideIndex++;
-
-    if(slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    slides[slideIndex -1].styledisplay = "block";
-
-    setTimeout(showSlides , 3000);
-};
-
-showSlides();
+$('bpoly()').mousedown(function() {
+    alert('clicou e segurou');
+}).bind('mouseup mouseleave', function() {
+    alert('soltou, "descliclou"');
+});
