fd8e3f50cbd7a3deac2f66df67e02091974b51f9
Jonathan Crispiniano <jonathancrispiniano@gmail.com>
2020-07-29 19:33:10 -0300

    Script JS para button da colmeia de especialidades

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,13 @@
-$('bpoly()').mousedown(function() {
-    alert('clicou e segurou');
-}).bind('mouseup mouseleave', function() {
-    alert('soltou, "descliclou"');
-});
+let a = 1
+
+function bpoly() {
+    if (a == 1) {
+        document.getElementById("button").innerHTML = "Lorem ipsum dolor sit amet";
+        document.getElementById("btgeral").style.webkitTransform = "scale(1.5)";
+        a = 2
+    } else {
+        document.getElementById("button").innerHTML = "Lorem";
+        document.getElementById("btgeral").style.webkitTransform = "";
+        a = 1
+    }
+};
