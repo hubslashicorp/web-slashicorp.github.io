874e342ea451efa53b6f3173f8195449ff370070
Jonathan Crispiniano <jonathancrispiniano@gmail.com>
2020-07-30 01:02:29 -0300

    Adicionado conteudo de especialidades

--- scripts/home.js
+++ scripts/home.js
@@ -2,12 +2,16 @@ let a = 1
 
 function bpoly() {
     if (a == 1) {
-        document.getElementById("button").innerHTML = "Lorem ipsum dolor sit amet";
-        document.getElementById("btgeral").style.webkitTransform = "scale(1.5)";
+        document.getElementById("button").innerHTML = "Atlassian Jira Align";
+        document.getElementById("btgeral").style.webkitTransform = "scale(5)";
+        document.getElementById("btgeral").style.fillOpacity = "90";
+        document.getElementById("texto").style.fontSize = "2"
         a = 2
     } else {
-        document.getElementById("button").innerHTML = "Lorem";
+        document.getElementById("button").innerHTML = "Aja";
         document.getElementById("btgeral").style.webkitTransform = "";
+        document.getElementById("btgeral").style.fillOpacity = "";
+        document.getElementById("texto").style.fontSize = ""
         a = 1
     }
 };
